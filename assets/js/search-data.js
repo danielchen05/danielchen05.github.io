f555b33079c6e7e468e9a2245c6a56961455777c
danielchen05 <155658017+danielchen05@users.noreply.github.com>
2026-09-06 19:17:27 +0000

    Deploying to gh-pages from @ danielchen05/danielchen05.github.io@3243bd1b982c4014b896a1bcf9b9aa17287aa5bf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -59,9 +59,9 @@ ninja.data = [{
           description: "Reusable preprocessing, QC, destriping, and visualization workflows for Visium HD data",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-",
+          title: '',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
