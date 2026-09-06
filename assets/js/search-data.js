58d56441900b4ad599ba178b01c00ad7a7c3d449
danielchen05 <155658017+danielchen05@users.noreply.github.com>
2026-09-06 19:18:12 +0000

    Deploying to gh-pages from @ danielchen05/danielchen05.github.io@71d0ea33c56a74c1dadb0c7409dc5ae98c2bd712 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -59,9 +59,9 @@ ninja.data = [{
           description: "Reusable preprocessing, QC, destriping, and visualization workflows for Visium HD data",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-",
-          title: '',
-          description: "",
+            },},{id: "projects-detecting-homozygous-deletions-from-single-cell-dna-sequencing-data",
+          title: 'Detecting Homozygous Deletions from Single-Cell DNA Sequencing Data',
+          description: "Statistical methods for detecting focal homozygous deletions from low-coverage single-cell DNA sequencing data",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
